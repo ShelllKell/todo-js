840a56d37735008b2be83ea69ba83f6cd3c31fd2
Shelby Kelly <shelby.kelly@colorado.edu>
2014-08-27 16:05:09 -0600

    line

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,33 +6,37 @@
 $(document).ready(function () {
 
 
-  $('body').append("<h1>Todo.ly</h1>");
-
-  $('body').append('<form><input type="text" id="item">');
-
-  $('body').append("<button>Create Todo</button>");
+$('body').append("<h1>Todo.ly</h1>");
+$('body').append('<form><input type="text" id="item">');
+$('body').append("<button>Create Todo</button>");
 
 
   $('button').one('click', function () {
-    $('body').append('<h2>My Todos</h2>')
+    $('body').append('<div class="line"></div><h2>Todo!</h2>')
   });
 
 
   $('button').click(function (e) {
     e.preventDefault();
     var item = $('#item').val();
-    $('body').append("<ul>" + item + "</ul>")
+    $('body').append('<ul>' + item + '</ul><span class="ex">X</span>')
     $('input').val("")
 
-    setTimeout(function () {
-      $('body').append('<div class="flash">Todo created</div>')
-      $('div').fadeOut(5000, function () {
-        $('div').remove();
-      });
 
+
+    setTimeout(function () {
+      $('.flash').remove();
+      $('h2').after('<div class="flash">Todo created<span class="ex">X</span></div>')
+      $('.flash').fadeOut(5000, function () {
+        $('.flash').remove();
+      });
+      $('.ex').click(function () {
+        $('.flash').remove();
+      });
     });
 
   });
+
 });
 
 
